c6172bd54e24ccc6e1d9fc9b92776be1c02914cf
PrabhashSwarnajith <prabashswarnajith2001@gmail.com>
2024-03-27 17:40:50 +0530

    add project

---
+++ src/main/java/lk/modulenest/module_management_system/ModuleManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.modulenest.module_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/modulenest/module_management_system/ModuleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.modulenest.module_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
